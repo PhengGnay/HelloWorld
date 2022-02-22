144d44082cb9d7a0604d1f32ebbeef78eb8ab25a
Pheng <phengyangster9@gmail.com>
2022-02-22 14:24:31 -0800

    first commit

---
+++ Chapter2/2-1.py
@@ -0,0 +1,20 @@
+# print(3 + 2, 3 - 2, 3 * 2)
+# print(8 / 2, 8 ** 2, 2*(3 + 4))
+
+# speed = 50
+# timeElasped = 14
+# distance = speed * timeElasped
+# print(distance)
+
+# round(2.317, 2)
+
+# a = 2
+# b = 3
+# print(abs(1 - (4 * b)))
+# print(int((a ** b) + .8))
+# print(round(a / b, 3))
+
+from mimetypes import init
+
+
+
