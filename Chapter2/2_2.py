462d85154fed47ea7855b0582d4cd4024f696b1a
Pheng <phengyangster9@gmail.com>
2022-02-24 13:31:44 -0800

    first commit

---
+++ Chapter2/2_2.py
@@ -0,0 +1,30 @@
+
+# myvar = "say it ain't so"
+# print (myvar)
+
+
+# var2 = "Spam & Eggs"
+
+# spam = "Spam"
+# eggs = "Eggs" 
+# spam_and_eggs = spam + "&" + eggs
+
+# print (spam_and_eggs[:])
+
+# length = len (spam_and_eggs)
+# print(length)
+
+# print(spam_and_eggs.upper())
+# print(spam_and_eggs.lower())
+
+# praise = "Good Doggie"
+# uppercase = praise.upper() 
+# count = praise.upper().count("G")
+# print("Count: ", count)
+
+# fullName = input("Enter a full name:")
+# n = fullName.rfind(" ")
+# print("Last name:",fullName[n+1:])
+# print("First name(s) :", fullName[ :n])
+
+
