876382429388944a1bd1e79a87b0759bdc7517ea
Pheng <phengyangster9@gmail.com>
2022-02-24 14:47:59 -0800

    first commit

--- Chapter2/2_2.py
+++ Chapter2/2_2.py
@@ -18,13 +18,37 @@
 # print(spam_and_eggs.lower())
 
 # praise = "Good Doggie"
+
 # uppercase = praise.upper() 
 # count = praise.upper().count("G")
 # print("Count: ", count)
+
+# n = index_of_space
 
 # fullName = input("Enter a full name:")
 # n = fullName.rfind(" ")
 # print("Last name:",fullName[n+1:])
 # print("First name(s) :", fullName[ :n])
 
+# annual_income = eval(input("Enter your annual income: "))
+# print("User's income is ", annual_income)
+# monthly_income = (annual_income/12)
+# print("monthly income", monthly_income)
+
+# var1 = 1
+# var2 = 2
+# var3 = 3
+# var4 = 4
+
+# var5 = var1+var2 # 3 Int
+# var6 = var3+var4 # 34 # String
+
+str1 = "Python"
+length = len(str1) #5 Int
+str2 = str1[:-1] #Python String
+print("str2", str2)
+str3 = str1[:10] #Python String
+print("str3", str3)
+str4 = str2[2:-1] #tho String
+print("str4", str4)
 
