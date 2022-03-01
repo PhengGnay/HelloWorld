e284656d9689a0b384c4014157734b7d468bb529
Pheng <phengyangster9@gmail.com>
2022-03-01 11:17:23 -0800

    adding 2_3

---
+++ Chapter2/2_3.py
@@ -0,0 +1,17 @@
+# print("one", "two", "three", sep=",")
+# print("Hello", end=" ") 
+# print("World!")
+
+# print("a\tb\tc") # a    b    c
+# print("a\tb\tc\nd\te\tf") # a   b   c
+#                           # d   e   f
+
+# print("Hello World!") # Hello World!
+# print("\"Hello World!\"") # "Hello World!"
+# print("\\Hello World!\\") # \Hello World!\
+
+print("0123456789012345678901234567")
+print("Rank".ljust(5), "Player".ljust(20), "HR".rjust(3), sep="")
+print('1'.center(5), "Barry Bonds".ljust(20), "762".rjust(3), sep="")
+print('2'.center(5), "Hank Aaron".ljust(20), "755".rjust(3), sep="")
+print('3'.center(5), "Babe Ruth".ljust(20), "714".rjust(3), sep="")
